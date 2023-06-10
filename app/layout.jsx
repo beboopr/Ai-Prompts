a7481f6291c5709873e804a49317020f5beaebc0
beboopr <linnets-router.09@icloud.com>
2023-06-10 18:41:38 -0400

    Nav & provider added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,8 @@
+// Here were going to add everything that is going to be re-usable across all pages
+
 import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
     title: "Promptopia",
@@ -9,13 +13,16 @@ const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
 
             <main className='app'>
+                <Nav />
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
   )
